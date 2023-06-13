7902af687686697bb4bb17ea103f99b716790d18
GabeOP <gabrieloliveirap1@hotmail.com>
2023-06-13 18:22:55 -0300

    method updateUser

--- API-spring-boot/src/main/java/com/api/resources/UserResource.java
+++ API-spring-boot/src/main/java/com/api/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,12 @@ public class UserResource {
 	public ResponseEntity<User> postUser(@RequestBody User obj) {
 		service.postUser(obj);
 		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User obj) {
+		service.updateUser(id, obj);
+		return ResponseEntity.status(HttpStatus.OK).body("Usuário atualizado com sucesso!");
 	}
 	
 	@DeleteMapping("/{id}")
--- API-spring-boot/src/main/java/com/api/services/UserService.java
+++ API-spring-boot/src/main/java/com/api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,20 @@ public class UserService {
 	
 	public User postUser(User obj) {
 		return repository.save(obj);
+	}
+	
+	public User updateUser(Long id, User obj) {
+		Optional<User> entity = repository.findById(id);
+		var userModel = entity.get();
+		
+		UpdatedData(userModel, obj);
+		
+		return repository.save(userModel);
+	}
+	
+	public void UpdatedData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setState(obj.getState());
 	}
 	
 	public void deleteUser(Long id) {
