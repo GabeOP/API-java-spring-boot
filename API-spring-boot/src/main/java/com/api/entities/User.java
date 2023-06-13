4820fc1d33a3c4d57169b743fe83294016cf7679
GabeOP <gabrieloliveirap1@hotmail.com>
2023-06-13 16:55:00 -0300

    Create User.java

---
+++ API-spring-boot/src/main/java/com/api/entities/User.java
@@ -0,0 +1,72 @@
+package com.api.entities;
+
+import java.sql.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String state;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created_at;
+	
+	public User() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
